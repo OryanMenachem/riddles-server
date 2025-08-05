72a2f0cdb8c4a7381de6a12a780df5c5c460d2ec
Oryan Menachem <om3160204@gmail.com>
2025-08-05 16:45:00 +0300

    feat: update riddle schema

--- src/riddle/riddle.schema.ts
+++ src/riddle/riddle.schema.ts
@@ -1,6 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+enum Difficulty {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard',
+}
+
 export type RiddleDocument = Riddle & Document;
 
 @Schema()
@@ -8,8 +14,8 @@ export class Riddle {
   @Prop({ required: true })
   category: string;
 
-  @Prop({ required: true })
-  difficulty: string;
+  @Prop({ required: true, enum: Difficulty })
+  difficulty: Difficulty;
 
   @Prop({ required: true })
   riddleDescription: string;

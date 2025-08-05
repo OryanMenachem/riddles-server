11f29802a5d6cad4bcc39f6ec85d9fa565c16f3f
Oryan Menachem <om3160204@gmail.com>
2025-08-05 17:39:11 +0300

    Implement createRiddle, findAllRiddles, findRiddleById, findRiddleByDifficulty, updateRiddleById, deleteRiddleById  functions in riddle service and controller

--- src/riddle/riddle.controller.ts
+++ src/riddle/riddle.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { RiddleService } from './riddle.service';
+import { CreateRiddleDto } from './dto/create-riddle.dto';
+import { Riddle } from './riddle.schema';
 
-@Controller('riddle')
-export class RiddleController {}
+@Controller('riddles')
+export class RiddleController {
+  constructor(private readonly riddleService: RiddleService) {}
+
+  @Post()
+  async createRiddle(
+    @Body() createRiddleDto: CreateRiddleDto,
+  ): Promise<Riddle> {
+    return this.riddleService.createRiddle(createRiddleDto);
+  }
+
+  @Get()
+  async findAllRiddles(): Promise<Riddle[]> {
+    return this.riddleService.findAllRiddles();
+  }
+  @Get(':id')
+  async findRiddleById(@Param('id') id: string): Promise<Riddle | null> {
+    return this.riddleService.findRiddleById(id);
+  }
+
+  @Get('difficulty/:difficulty')
+  async findRiddleByDifficulty(
+    @Param('difficulty') difficulty: string,
+  ): Promise<Riddle[]> {
+    return this.riddleService.findRiddleByDifficulty(difficulty);
+  }
+
+  @Post('update/:id')
+  async updateRiddleById(
+    @Body() updateData: Partial<CreateRiddleDto>,
+    @Body('id') id: string,
+  ): Promise<Riddle | null> {
+    return this.riddleService.updateRiddleById(id, updateData);
+  }
+
+  @Post('delete/:id')
+  async deleteRiddleById(@Param('id') id: string): Promise<Riddle | null> {
+    return this.riddleService.deleteRiddleById(id);
+  }
+}
--- src/riddle/riddle.service.ts
+++ src/riddle/riddle.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Riddle, RiddleDocument } from './riddle.schema';
+import { CreateRiddleDto } from './dto/create-riddle.dto';
 
 @Injectable()
-export class RiddleService {}
+export class RiddleService {
+  constructor(
+    @InjectModel(Riddle.name) private riddleModel: Model<RiddleDocument>,
+  ) {}
+
+  async createRiddle(createRiddleDto: CreateRiddleDto): Promise<Riddle> {
+    const createdRiddle = new this.riddleModel(createRiddleDto);
+    return createdRiddle.save();
+  }
+
+  async findAllRiddles(): Promise<Riddle[]> {
+    return this.riddleModel.find().exec();
+  }
+  async findRiddleById(id: string): Promise<Riddle | null> {
+    return this.riddleModel.findById(id).exec();
+  }
+
+  async findRiddleByDifficulty(difficulty: string): Promise<Riddle[]> {
+    return this.riddleModel.find({ difficulty }).exec();
+  }
+
+  async updateRiddleById(
+    id: string,
+    updateData: Partial<CreateRiddleDto>,
+  ): Promise<Riddle | null> {
+    return this.riddleModel
+      .findByIdAndUpdate(id, updateData, { new: true })
+      .exec();
+  }
+
+  async deleteRiddleById(id: string): Promise<Riddle | null> {
+    return this.riddleModel.findByIdAndDelete(id).exec();
+  }
+}

2666b5ce467e0a3c662f9272231ceb2e98a0d9fd
Oryan Menachem <om3160204@gmail.com>
2025-08-05 15:57:29 +0300

    feat: create supabase module and service, connect to supabase and mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { PlayerModule } from './player/player.module';
 import { RiddleModule } from './riddle/riddle.module';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().uri().required(),
+        SUPABASE_URI: Joi.string().uri().required(),
+        SUPABASE_ANON_KEY: Joi.string().required(),
+      }),
     }),
-    PlayerModule,
+
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
     RiddleModule,
+    PlayerModule,
+    SupabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],

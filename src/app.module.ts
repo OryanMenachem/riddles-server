1ee38dae785514880ec37a5ab694e0ed9b4fa864
Oryan Menachem <om3160204@gmail.com>
2025-08-05 15:29:29 +0300

    feat: add generated player and riddle modules, controllers, and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PlayerModule } from './player/player.module';
+import { RiddleModule } from './riddle/riddle.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PlayerModule,
+    RiddleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
